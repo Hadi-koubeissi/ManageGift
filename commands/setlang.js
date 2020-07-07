aee4f09b70a1506cd8bbe4dde334107ff041c195
Hadi <hadikoubeissi40@gmail.com>
2020-07-07 16:02:56 +0200

    add 🇺🇦 language 📰📌

--- commands/setlang.js
+++ commands/setlang.js
@@ -1,58 +1,52 @@
-const ms = require('ms');
-
 exports.run = async (client, message, args) => {
-
-    const db = require('quick.db')
-
-    const config = require(`../config.js`)
-
+    const db = require("quick.db")
+    const config = require("../config.js")
     let language = db.fetch(`language_${message.guild.id}`)
-
-    if (language === null) language = config.basiclang
-
+    if(language === null) language = config.basiclang
     const lang = require(`../language/${language}.js`)
+    if(!args[0]) return message.channel.send(lang.lang.msg)
 
-    if (!args[0]) return message.channel.send(lang.lang.msg)
-
-    if (args[0] === "ar") {
-
-        if (!message.member.hasPermission("MANAGE_CHANNELS")) {
+    // Arabic lang
+    if(args[0] === "ar"){
+        if(!message.member.hasPermission("MANAGE_CHANNELS")){
             return message.channel.send(lang.lang.perms)
         }
 
-        if (language === "ar") return message.channel.send(lang.lang.err)
-
+        if(language === "ar") return message.channel.send(lang.lang.err)
         db.set(`language_${message.guild.id}`, "ar")
-
         message.channel.send(":flag_lb: | لغة هذا السيرفر هي العربية الآن!")
-
     }
 
-    if (args[0] === "en") {
-
-        if (!message.member.hasPermission("MANAGE_CHANNELS")) {
+    // English lang
+    if(args[0] === "en"){
+        if(!message.member.hasPermission("MANAGE_CHANNELS")){
             return message.channel.send(lang.lang.perms)
         }
 
-        if (language === "en") return message.channel.send(lang.lang.err)
-
+        if(language === "en") return message.channel.send(lang.lang.err)
         db.set(`language_${message.guild.id}`, "en")
-
         message.channel.send(":flag_gb: | The language of this server is now English!")
-
     }
 
-    if (args[0] === "ru") {
-
-        if (!message.member.hasPermission("MANAGE_CHANNELS")) {
+    // Russian lang
+    if(args[0] === "ru"){
+        if(!message.member.hasPermission("MANAGE_CHANNELS")){
             return message.channel.send(lang.lang.perms)
         }
 
-        if (language === "ru") return message.channel.send(lang.lang.err)
-
+        if(language === "ru") return message.channel.send(lang.lang.err)
         db.set(`language_${message.guild.id}`, "ru")
+        message.channel.send(":flag_ru: | Язык изменён на Русский!")
+    }
 
-        message.channel.send(":flag_ru: | Язык установлен на Русский!")
+    // Ukranian lang
+    if(args[0] === "ua"){
+        if(!message.member.hasPermission("MANAGE_CHANNELS")){
+            return message.channel.send(lang.lang.perms)
+        }
 
+        if(language === "ua") return message.channel.send(lang.lang.err)
+        db.set(`language_${message.guild.id}`, "ua")
+        message.channel.send(":flag_ua: | Мову змінено на Українську!")
     }
 }
