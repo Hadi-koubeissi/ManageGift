64b54a089eed1f21bffa0f31b852ab266f7b876f
Hadi <hadikoubeissi40@gmail.com>
2020-07-07 16:10:27 +0200

    fixs error in Info Commands 🐛📢

--- commands/ping.js
+++ commands/ping.js
@@ -1,18 +1,9 @@
-const ms = require('ms');
-const Discord = require('discord.js')
-
-exports.run = async (client, message, args) => {
-
-    const db = require('quick.db')
-
-    const config = require(`../config.js`)
-
+exports.run = async (client, message) => {
+    const db = require("quick.db")
+    const config = require("../config.js")
     let language = db.fetch(`language_${message.guild.id}`)
-
-    if (language === null) language = config.basiclang
-
+    if(language === null) language = config.basiclang
     const lang = require(`../language/${language}.js`)
 
-    var ping = Date.now() - message.createdTimestamp;
     message.channel.send(lang.info.ping + `${Date.now() - message.createdTimestamp}`);
 }
