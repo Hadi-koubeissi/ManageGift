d2b284c44b9baecda03f642a4c0b08a27d1b631b
Hadi <hadikoubeissi40@gmail.com>
2020-07-07 15:40:49 +0200

    some edit 📝📌

--- config.sample.js
+++ config.sample.js
@@ -1,80 +1,62 @@
 module.exports = {
-    token: "XXXXX", //token bot | توكن البوت
+    token: "", // token bot | توكن البوت
 	
-	prefix: "XXXX", //prefix bot | برفكس البوت
+	prefix: "", // prefix bot | برفكس البوت
 	
-	idbot: "XXXX", //Bot ID | ايدي البوت
+	idbot: "", // Bot ID | ايدي البوت
 
-    hostedBy: true,
+    hostedBy: true, // Why hosted gift | لماذا استضافت الهدية
 
-	basiclang: "en", //basic lang | اللغة الاساسية
+	basiclang: "en", // Default lang | اللغة الاساسية
 	
 	auth: {
-		support: "XXXX" //support server URL | رابط سيرفر السيبورت
+		support: "" // Support server URL | رابط سيرفر السيبورت
 	},
 
+	// Status | الحالات
 	status: [
 		{
-			name: `%ghelp | ManageGift's on {server} servers`,
+			name: `%ghelp | ManageGift's on {server} servers!`,
 			type: "LISTENING"
-		},// الحالات | status
+		},
 		{
-			name: "https://github.com/Hadi-Koubeissi/GiveawayBoT.",
+			name: "https://github.com/Hadi-Koubeissi/ManageGift",
 			type: "PLAYING"
 		}
 	],
 
 	embeds: {
-        color: "#FF4500", //Embed color | لون الامبد
-        footers: "🎁 ManageGift's | http://managegift.ga" //Embed footer | الكتابة لي تكون اخر الامبد
+        color: "#FF4500", // Embed color | لون الامبد
+        footers: "🎁 ManageGift's | http://managegift.ga" // Embed footer | الكتابة لي تكون اخر الامبد
     },
 
     events: {
-        addcolor: "#DC143C", //The color of the event add | لون الامبد
-		remcolor: "RED" //The color of the event remove  
-		
+        addcolor: "#DC143C", // The color of the event add | لون الامبد
+		remcolor: "RED" // The color of the event remove  
     },
 
-    reaction: "🎉", //Reaction to the giveaways if you in the console you see 'unknown emoji' that's what this emoji is not recognized by Discord | الايموجي الي بس تضغط عليها يصير القيف اوي اذا طلع لك ايرور بالكونسول يعني الايموجي ما تعرف بالديسكورد
+    reaction: "🎉", // Reaction to the giveaways if you in the console you see 'unknown emoji' that's what this emoji is not recognized by Discord | الايموجي الي بس تضغط عليها يصير القيف اوي اذا طلع لك ايرور بالكونسول يعني الايموجي ما تعرف بالديسكورد
 
-    grole: "Giveaway Manger", //If the member doesn't have permission to handle messages he can still use the giveaways commands if he has the role configured right here | اذا سويت الرتبة واعطيتها لحد يصير يقدر يسوي قيف اوي ويستخدم جميع اوامر البوت بدون صلاحيات
+    grole: "Giveaway Manger", // If the member doesn't have permission to handle messages he can still use the giveaways commands if he has the role configured right here | اذا سويت الرتبة واعطيتها لحد يصير يقدر يسوي قيف اوي ويستخدم جميع اوامر البوت بدون صلاحيات
 
-    //all emojis in ManageGift support server | الاينوحيات بتلقوها بروم في السيبورت سيرفر الخاص بالبوت
+    // All emojis in ManageGift support server | الاينوحيات بتلقوها بروم في السيبورت سيرفر الخاص بالبوت
 	emojis: {
-
-	success: "XXXX",
-
-	error: "XXXX",
-	
-	gift: "XXXX",
-
-	end: "XXXX",
-
-	time: "XXXX",
-
-	hoste: "XXXX",
-
-	ping: "XXXX",
-
-	link: "XXXX",
-
-	add: "XXXX",
-
-	vote: "XXXX",
-
-	supp: "XXXX",
-
-	command: "XXXX",
-
-	featured: "XXXX",
-
-	right: "XXXX",
-
-	stats: "XXXX",
-
-	ver: "XXXX",
-	
-	info: "XXXX"
+		success: "",
+		error: "",
+		gift: "",
+		end: "",
+		time: "",
+		hoste: "",
+		ping: "",
+		link: "",
+		add: "",
+		vote: "",
+		supp: "",
+		command: "",
+		featured: "",
+		right: "",
+		stats: "",
+		ver: "",
+		info: ""
 	}
-
 }
