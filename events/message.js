a4b8c65d994d66b434614ed3ca1c19189681fc58
Hadi <hadikoubeissi40@gmail.com>
2020-07-08 16:06:14 +0200

    add setprefix cmd to message event 🚩✔

--- events/message.js
+++ events/message.js
@@ -1,12 +1,29 @@
+const config = require('../config');
+const db = require('quick.db');
+
 module.exports = (client, message) => {
-  if(!message.channel.permissionsFor(client.user.id).has('SEND_MESSAGES')) { return; }
-  if(message.author.bot) return;
-  if(message.content.indexOf(client.config.prefix) !== 0) return;
+
+  //for setprefix command 
+  let prefix;
+  let prefixes = db.fetch(`prefix_${message.guild.id}`);
+  if(prefixes == null) {
+    prefix = config.prefix 
+  } else {
+    prefix = prefixes;
+  }
+  //now we done prefix fetching for guilds
+  if (!message.content.startsWith(prefix) || !message.guild) return;
+
+  //Our standard argument/command name definition.
   const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
+
+  //Grab the command data from the client.commands Enmap
   const cmd = client.commands.get(command);
+
+  //If that command doesn't exist, silently exit and do nothing
   if (!cmd) return;
 
-  // Run the command
+  //Run the command
   cmd.run(client, message, args);
 };
