a55c1be4a1d997713604cbe01490461c5789b35b
Hadi <hadikoubeissi40@gmail.com>
2020-08-27 17:36:55 +0200

    🔨 edit edit command

--- commands/Giveaways/edit.js
+++ commands/Giveaways/edit.js
@@ -1,5 +1,6 @@
 exports.run = async (client, message, args) => {
     const ms = require("ms");
+    const num = require('num-parse')
     const db = require("quick.db")
     const config = require("../../config.js")
     let language = db.fetch(`language_${message.guild.id}`)
@@ -8,47 +9,39 @@ exports.run = async (client, message, args) => {
     let role = db.fetch(`role_${message.guild.id}`)
     if(role === null) role = config.grole
 
-    // If the member doesn't have enough permissions
     if(!message.member.hasPermission("MANAGE_MESSAGES") && !message.member.roles.cache.some((r) => r.name === role)){
-        return message.channel.send(lang.start.perms + "** **" + "`" + role + "`" + ".");
+        return message.channel.send(lang.start.perms + "** **" + "`" + role + "`" + "!.");
     }
-    
-    // If no message ID or giveaway name is specified
-    if(!args[0]){
-        return message.channel.send(lang.edit.msg);
-    }
-
-    // try to found the giveaway with prize then with ID
-    let giveaway = client.giveawaysManager.giveaways.find((g) => g.prize === args.join(" ")) || client.giveawaysManager.giveaways.find((g) => g.messageID === args[0]);
-    
-    // If no giveaway was found
-    if(!giveaway){
+    let id = args[0];
+    if (!id) return message.channel.send(lang.edit.msg);
+    let hasGiveaway = client.giveawaysManager.giveaways.find((g) => g.messageID === id);
+    if (!hasGiveaway) {
         return message.channel.send(lang.edit.err + "** **" + "`" + args.join(" ") + "`" + ".");
     }
-
-    // Number of winners
-    let giveawayNumberWinners = args[1];
-    // If the specified number of winners is not a number
-    if(isNaN(giveawayNumberWinners)){
-        return message.channel.send(lang.edit.argswinners);
+    let time = args[1];
+    if (!time) return message.channel.send(lang.edit.time);
+    if (ms(time) > ms("10d")) {
+        return message.channel.send(lang.edit.errtime);
     }
+    let winners = args[2];
+    if (!winners) return message.channel.send(lang.edit.argswinners);
+    num(winners, 1);
+    if (winners > 15) return message.channel.send(lang.edit.errwinners);
+    let prize = args.slice(3).join(" ");
+    if (!prize) return message.channel.send(lang.edit.prize);
 
-    // Giveaway prize
-    let giveawayPrize = args.slice(2).join(" ");
-    // If no prize is specified
-    if(!giveawayPrize){
-        return message.channel.send(lang.edit.prize);
-    }
-
-    let messageID = args[0];
-    client.giveawaysManager.edit(messageID, {
-        newWinnerCount: giveawayNumberWinners,
-        newPrize: giveawayPrize,
-        addTime: 5000
-    }).then(() => {
-        message.channel.send(lang.edit.good);
+    client.giveawaysManager.edit(hasGiveaway.messageID, {
+        addTime: ms(time),
+        newWinnerCount: parseInt(winners),
+        newPrize: prize,
+    })
+    message.channel.send(lang.edit.good) .then((msg) => {
+        msg.delete({ timeout: 10000 })
+    })
+    .then(() => {
+        if (message.deletable) message.delete({ timeout: 10000 });
+        return;
     }).catch((err) => {
-        console.log(err)
         message.channel.send(lang.edit.errmod);
     });
 }
