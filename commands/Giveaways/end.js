0463b97976f295efc8a3b927a5962cc7ba741356
TheMaestroo <61483023+TheMaestro0@users.noreply.github.com>
2020-09-15 01:55:06 +0200

    Update end.js

--- commands/Giveaways/end.js
+++ commands/Giveaways/end.js
@@ -1,30 +1,22 @@
-exports.run = async (client, message, args) => {
-    const ms = require("ms");
-    const db = require("quick.db")
-    const config = require("../../config.js")
-    let language = db.fetch(`language_${message.guild.id}`)
-    if (language === null) language = config.basiclang
-    const lang = require(`../../language/${language}.js`)
-    let role = db.fetch(`role_${message.guild.id}`)
-    if (role === null) role = config.grole
+const ms = require("ms");
 
+exports.run = async (client, message, args, lang) => {
+    let role = client.db.fetch(`role_${message.guild.id}`);
+    if (!role) role = client.config.grole;
+    
     // If the member doesn't have enough permissions
-    if (!message.member.hasPermission("MANAGE_MESSAGES") && !message.member.roles.cache.some((r) => r.name === role)) {
-        return message.channel.send(lang.start.perms + "** **" + "`" + role + "`" + ".");
-    }
-
+    if (!message.member.hasPermission("MANAGE_MESSAGES") && !message.member.roles.cache.some((r) => r.name === role)) return message.channel.send(lang.start.perms + "** **" + "`" + role + "`" + ".");
+    
     // If no message ID or giveaway name is specified
-    if (!args[0]) {
-        return message.channel.send(lang.end.msg);
-    }
+    if (!args[0]) return message.channel.send(lang.end.msg);
+    
 
     // try to found the giveaway with prize then with ID
     let giveaway = client.giveawaysManager.giveaways.find((g) => g.prize === args.join(' ')) || client.giveawaysManager.giveaways.find((g) => g.messageID === args[0]);
 
     // If no giveaway was found
-    if (!giveaway) {
-        return message.channel.send(lang.end.err + "** **" + "`" + args.join(' ') + "`" + ".");
-    }
+    if (!giveaway) return message.channel.send(lang.end.err + "** **" + "`" + args.join(' ') + "`" + ".");
+    
 
     // Edit the giveaway
     client.giveawaysManager.edit(giveaway.messageID, {
@@ -34,8 +26,7 @@ exports.run = async (client, message, args) => {
         .then(() => {
             // Success message
             message.channel.send(lang.end.good + "** **" + "`" + (client.giveawaysManager.options.updateCountdownEvery / 1000) + "`" + "** **" + lang.units.seconds + ".");
-        })
-        .catch((e) => {
+        }).catch((e) => {
             if (e.startsWith(`Giveaway with message ID ${giveaway.messageID} is not ended.`)) {
                 message.channel.send(lang.end.err + "** **" + "`" + giveaway.messageID + "`" + ".");
             } else {
