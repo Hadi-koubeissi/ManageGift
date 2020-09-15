2ff314a7a987b03ed5781b881a5657ce3f4d0af1
TheMaestroo <61483023+TheMaestro0@users.noreply.github.com>
2020-09-15 02:08:53 +0200

    Update stats.js

--- commands/Info/stats.js
+++ commands/Info/stats.js
@@ -1,20 +1,16 @@
-exports.run = async (client, message) => {
-    const Discord = require("discord.js")
-    const db = require("quick.db")
-    const moment = require('moment')
-    require('moment-duration-format')
-    const config = require("../../config.js")
-    let language = db.fetch(`language_${message.guild.id}`)
-    if (language === null) language = config.basiclang
-    const lang = require(`../../language/${language}.js`)
+const { MessageEmbed } = require("discord.js")
+const moment = require('moment')
+require('moment-duration-format')
+
+exports.run = async (client, message, args, lang) => {
     let version = require("../../package.json").version;
     let discord_giveaways = require("../../package.json").dependencies["discord-giveaways"];
-    let supportURL = config.auth.support;
+    let supportURL = client.config.auth.support;
     let web = "http://managegift.ga";
     let donate = "https://paypal.me/Hadikob";
     let invite = "https://discordapp.com/oauth2/authorize?client_id=598564396691750933&scope=bot&permissions=8";
     const duration = moment.duration(client.uptime).format(" D [**days**], H [**hours**], m [**minutes**], s [**seconds**]");
-    let embed = new Discord.MessageEmbed()
+    message.channel.send(new MessageEmbed()
         .setTitle(lang.stats.title)
         .setAuthor(`ManageGift's | Version ${version}`)
         .setThumbnail("https://g.top4top.io/s_1695qf6p11.gif")
@@ -25,7 +21,6 @@ exports.run = async (client, message) => {
         .addField(lang.stats.on, lang.stats.for + `${duration}`)
         .addField(lang.stats.moreinfo, lang.stats.comd + message.client.commands.size + `\n` + lang.stats.giv + client.giveawaysManager.giveaways.length)
         .addField(lang.help.link, `[Support server](${supportURL}) ● [Donate](${donate}) ● [Website](${web}) ● [Invite](${invite})`)
-        .setColor(config.embeds.color)
-        .setFooter(config.embeds.footers)
-    message.channel.send(embed);
+        .setColor(client.config.embeds.color)
+        .setFooter(client.config.embeds.footers));
 }
