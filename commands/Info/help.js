6574c6c49cbdbdfbb62c851dcf2d4e3397b669ab
TheMaestroo <61483023+TheMaestro0@users.noreply.github.com>
2020-09-15 02:06:25 +0200

    Update help.js

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -1,17 +1,12 @@
-exports.run = async (client, message) => {
-    const Discord = require("discord.js")
-    const db = require("quick.db")
-    const config = require("../../config.js")
-    let language = db.fetch(`language_${message.guild.id}`)
-    if (language === null) language = config.basiclang
-    const lang = require(`../../language/${language}.js`)
+const { MessageEmbed } = require("discord.js");
+exports.run = async (client, message, args, lang) => {
     let version = require("../../package.json").version;
     let discord_giveaways = require("../../package.json").dependencies["discord-giveaways"];
-    let supportURL = config.auth.support;
+    let supportURL = client.config.auth.support;
     let web = "http://managegift.ga";
     let donate = "https://paypal.me/Hadikob";
     let invite = "https://discordapp.com/oauth2/authorize?client_id=598564396691750933&scope=bot&permissions=8";
-    let embed = new Discord.MessageEmbed()
+    message.channel.send(new MessageEmbed()
         .setTitle(lang.help.title)
         .setAuthor(`ManageGift's | Version ${version}`)
         .setThumbnail('https://i.top4top.io/p_1603lgj9t1.gif')
@@ -20,7 +15,5 @@ exports.run = async (client, message) => {
         .addField(lang.help.cmm, lang.help.cmdd)
         .addField(lang.help.link, `[Support server](${supportURL}) ● [Donate](${donate}) ● [Website](${web}) ● [Invite](${invite})`)
         .setColor(config.embeds.color)
-        .setFooter(config.embeds.footers)
-
-    message.channel.send(embed)
+        .setFooter(config.embeds.footers));
 }
