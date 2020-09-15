becf2d648aba85fd96507600d5291c818f45ed27
TheMaestroo <61483023+TheMaestro0@users.noreply.github.com>
2020-09-15 02:20:25 +0200

    Update setprefix.js

--- commands/Advanced cmd/setprefix.js
+++ commands/Advanced cmd/setprefix.js
@@ -1,14 +1,5 @@
-exports.run = async (client, message, args) => {
-
-    const config = require('../../config');
-    const db = require('quick.db');
-
-    let language = db.fetch(`language_${message.guild.id}`)
-    if (language === null) language = config.basiclang
-    const lang = require(`../../language/${language}.js`);
-
+exports.run = async (client, message, args, lang) => {
     if (!args[0]) return message.reply(lang.prefixerror);
-    await db.set(`prefix_${message.guild.id}`, args[0])
+    await client.db.set(`prefix_${message.guild.id}`, args[0]);
     message.channel.send(lang.setprefix + `\`${args[0]}\``);
-
 };
