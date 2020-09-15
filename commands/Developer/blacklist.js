485b3006786bd66659b88cffdc195e612c742c68
TheMaestroo <61483023+TheMaestro0@users.noreply.github.com>
2020-09-15 02:12:25 +0200

    Update blacklist.js

--- commands/Developer/blacklist.js
+++ commands/Developer/blacklist.js
@@ -1,39 +1,30 @@
-const Discord = require('discord.js'),
-  db = require('quick.db'),
-  config = require('../../config');
+const { MessageEmbed } = require('discord.js');
+
 exports.run = async (client, message, args) => {
-  if (message.author.id !== client.config.OwnerID) return message.reply("you do not have permission to use this command!")
-  const user = message.mentions.users.first()
-  if (!user) return message.reply("❌ | **Please mention someone!**")
+  if (message.author.id !== client.config.OwnerID) return message.reply("you do not have permission to use this command!");
+  const user = message.mentions.users.first();
+  if (!user) return message.reply("❌ | **Please mention someone!**");
+  if (user.id === message.author.id)return message.reply("❌ | **You cannot give yourself a blacklist**");
 
-  if (user.id === message.author.id) {
-    return message.reply("❌ | **You cannot give yourself a blacklist**");
-  }
-
-  let blacklist = await db.fetch(`blacklist_${user.id}`)
-
-  if (blacklist === "Not") {
-    db.set(`blacklist_${user.id}`, "Blacklisted")
-    let embed = new Discord.MessageEmbed()
+let blacklist = await client.db.fetch(`blacklist_${user.id}`);
+if (blacklist === "Not") {
+client.db.set(`blacklist_${user.id}`, "Blacklisted")
+message.channel.send(new MessageEmbed()
       .setTitle(':beginner: New User In Blacklist :beginner:')
       .setDescription(`:octagonal_sign: | ${user} has been blacklisted! \n By: ${message.author}`)
       .setColor('RED')
       .setFooter(client.config.embeds.footers)
-      .setTimestamp()
-    message.channel.send(embed)
+      .setTimestamp());
   } else if (blacklist === "Blacklisted") {
-    db.set(`blacklist_${user.id}`, "Not")
-    let embed = new Discord.MessageEmbed()
+    client.db.set(`blacklist_${user.id}`, "Not");
+message.channel.send(new MessageEmbed()
       .setTitle(':beginner: New User Has Unblacklisted :beginner:')
       .setDescription(`:white_circle: | ${user} has been unblacklisted!`)
       .setColor('GREEN')
       .setFooter(client.config.embeds.footers)
-      .setTimestamp()
-    message.channel.send(embed)
+      .setTimestamp());
   } else {
-    db.set(`blacklist_${user.id}`, "Not")
-    let embed = new Discord.MessageEmbed()
-      .setDescription(`Set up data for ${user}!`)
-    message.channel.send(embed)
+    client.db.set(`blacklist_${user.id}`, "Not");
+message.channel.send(new MessageEmbed().setDescription(`Set up data for ${user}!`));
   }
 }
