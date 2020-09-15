009ce796ee5bdd44b683b0c1846e679e679b0bbc
TheMaestroo <61483023+TheMaestro0@users.noreply.github.com>
2020-09-15 02:04:18 +0200

    Update invite.js

--- commands/Info/invite.js
+++ commands/Info/invite.js
@@ -1,21 +1,13 @@
-exports.run = async (client, message, args) => {
-    const Discord = require("discord.js")
-    const db = require("quick.db")
-    const config = require("../../config.js")
-    let language = db.fetch(`language_${message.guild.id}`)
-    if (language === null) language = config.basiclang
-    const lang = require(`../../language/${language}.js`)
+const { MessageEmbed } = require("discord.js");
 
+exports.run = async (client, message, args, lang) => {
     let websiteURL = `http://managegift.ga`;
-    let inviteLink = `https://discordapp.com/oauth2/authorize?client_id=${config.idbot}&scope=bot&permissions=8`;
-    let voteURL = `https://discordbots.org/bot/${config.idbot}/vote`;
-    let supportURL = config.auth.support
+    let inviteLink = `https://discordapp.com/oauth2/authorize?client_id=${client.config.idbot}&scope=bot&permissions=8`;
+    let voteURL = `https://discordbots.org/bot/${client.config.idbot}/vote`;
+    let supportURL = client.config.auth.support
+    if (args[0] == "copy")return message.channel.send(inviteLink);
 
-    if (args[0] === "copy") {
-        return message.channel.send(inviteLink);
-    }
-
-    let embed = new Discord.MessageEmbed()
+    message.channel.send(new MessageEmbed()
         .setAuthor(lang.invite.main)
         .setDescription(lang.invite.disc)
         .addField(lang.invite.web, (`[Click Here](${websiteURL})`))
@@ -23,8 +15,6 @@ exports.run = async (client, message, args) => {
         .addField(lang.invite.vote, (`[Click Here](${voteURL})`))
         .addField(lang.invite.sup, (`[Click Here](${supportURL})`))
         .setColor(config.embeds.color)
-        .setFooter(config.embeds.footers);
-
-    message.channel.send(embed);
+        .setFooter(config.embeds.footers));
 
 }
