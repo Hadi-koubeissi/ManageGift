98cef7d61d462e035808f46d9d1f385ca1529c50
TheMaestroo <61483023+TheMaestro0@users.noreply.github.com>
2020-09-15 02:18:07 +0200

    Update set.js

--- commands/Advanced cmd/set.js
+++ commands/Advanced cmd/set.js
@@ -1,9 +1,4 @@
-exports.run = async (client, message, args) => {
-    const db = require("quick.db")
-    const config = require("../../config.js")
-    let language = db.fetch(`language_${message.guild.id}`)
-    if (language === null) language = config.basiclang
-    const lang = require(`../../language/${language}.js`)
+exports.run = async (client, message, args, lang) => {
     if (!args[0]) return message.channel.send(lang.set.msg)
 
     if (args.join(" ") === "mention") {
@@ -18,12 +13,10 @@ exports.run = async (client, message, args) => {
         if (!message.member.hasPermission("MANAGE_CHANNELS")) {
             return message.channel.send(lang.set.perms)
         }
-
-        let mention = db.fetch(`mention_${message.guild.id}`)
-        if (mention === null) mention = false
-        if (mention === false) return message.channel.send(lang.set.erroff)
-        db.set(`mention_${message.guild.id}`, false)
-
+        
+        let mention = client.db.fetch(`mention_${message.guild.id}`);
+        if (!mention) return message.channel.send(lang.set.erroff);
+        client.db.set(`mention_${message.guild.id}`, false);
         message.channel.send(lang.set.moff)
     }
 
@@ -32,11 +25,9 @@ exports.run = async (client, message, args) => {
             return message.channel.send(lang.set.perms)
         }
 
-        let mention = db.fetch(`mention_${message.guild.id}`)
-        if (mention === null) mention = false
-        if (mention === true) return message.channel.send(lang.set.erron)
-        db.set(`mention_${message.guild.id}`, true)
-
+        let mention = client.db.fetch(`mention_${message.guild.id}`);
+        if (mention) return message.channel.send(lang.set.erron);
+        client.db.set(`mention_${message.guild.id}`, true);
         message.channel.send(lang.set.mon)
     }
 
@@ -44,12 +35,10 @@ exports.run = async (client, message, args) => {
         if (!message.member.hasPermission("MANAGE_CHANNELS")) {
             return message.channel.send(lang.set.perms)
         }
-
-        let logs = db.fetch(`logs_${message.guild.id}`)
-        if (logs === null) logs = false
+        
         var channel = message.mentions.channels.first();
-        if (!channel) return message.channel.send(lang.set.channel)
-        db.set(`logs_${message.guild.id}`, channel.id)
+        if (!channel) return message.channel.send(lang.set.channel);
+        client.db.set(`logs_${message.guild.id}`, channel.id);
 
         message.channel.send(lang.set.chon + "** **" + "`" + channel.name + "`" + ".")
     }
@@ -59,11 +48,9 @@ exports.run = async (client, message, args) => {
             return message.channel.send(lang.set.perms)
         }
 
-        let edit = db.fetch(`role_${message.guild.id}`)
-        if (edit === null) edit = false
         var role = message.mentions.roles.first();
-        if (!role) return message.channel.send(lang.set.role)
-        db.set(`role_${message.guild.id}`, role.name)
+        if (!role) return message.channel.send(lang.set.role);
+        client.db.set(`role_${message.guild.id}`, role.name);
 
         message.channel.send(lang.set.ron + "** **" + "`" + role.name + "`" + ".")
 
