4e28dbed7bdc449ca18372c560f4adf2393ecb54
Hadi <hadikoubeissi40@gmail.com>
2020-09-27 16:36:48 +0200

    🎨 beautifying help command

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -11,8 +11,10 @@ exports.run = async (client, message, args, lang) => {
         .setAuthor(`ManageGift's | Version ${version}`)
         .setThumbnail('https://i.top4top.io/p_1603lgj9t1.gif')
         .setDescription(lang.help.disc)
-        .addField(lang.help.cm, lang.help.cmd)
-        .addField(lang.help.cmm, lang.help.cmdd)
+        .addField(lang.help.giveawaycmd,'`create`, `start`, `end`, `edit`, `reroll`, `delete`')
+        .addField(lang.help.featuredcmd,'`setlang`, `set logs`, `set mention on`, `set mention off`, `setprefix`, `set role`')
+        .addField(lang.help.infocmd,'`stats`, `ping`, `invite`, `help`')
+        .addField(lang.help.ownerbot, '`eval`, `blacklist`')
         .addField(lang.help.link, `[Support server](${supportURL}) ● [Donate](${donate}) ● [Website](${web}) ● [Invite](${invite})`)
         .setColor(client.config.embeds.color)
         .setFooter(client.config.embeds.footers));
